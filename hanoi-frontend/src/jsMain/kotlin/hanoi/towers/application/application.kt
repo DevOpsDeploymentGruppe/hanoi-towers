52f1993a0238ff5bef3dee34cc2ad30963a0ad40
Schwalb Sophia <ss513@hdm-stuttgart.de>
2024-06-16 16:17:58 +0200

    test ci pipeline

--- hanoi-frontend/src/jsMain/kotlin/hanoi/towers/application/application.kt
+++ hanoi-frontend/src/jsMain/kotlin/hanoi/towers/application/application.kt
@@ -12,7 +12,7 @@ import org.jetbrains.compose.web.renderComposable
 
 @Markup
 @Suppress("FunctionName")
-fun Application() = renderComposable(rootElementId = "root") {
+fun Application() = renderComposable(rootElementId = "root") { //test
     Store({ Storage() }) {
         when( langLoaded() ) {
             true -> UI(this)
