bd9e12a181460e204d0c4c1eda814a98c269203a
knosvoxel <niklas.maeckle@gmail.com>
2024-06-17 15:49:49 +0200

    Removed test comment

--- hanoi-backend/src/main/kotlin/hanoi/towers/application/Application.kt
+++ hanoi-backend/src/main/kotlin/hanoi/towers/application/Application.kt
@@ -9,7 +9,6 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
 
-// Test
 fun Application.hanoi() {
 
     install(CORS) {
